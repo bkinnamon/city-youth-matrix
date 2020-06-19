e2ee668047ecd59760e8248a089b316ac274ddce
Brittany <bdb5075@gmail.com>
2020-06-19 15:24:06 -0400

    added Login and Events

---
+++ src/views/Login.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+import logo from '../CYM_logo_v1.svg'
+import '../App.css'
+
+function Login() {
+	return (
+	<div>
+		<div className = "img-container">
+			<img src={logo} alt = "City Youth Matrix logo" className = "main-logo"/>
+		</div>
+
+		<div className = "login-form" >
+			<div className = "container">
+				<div>
+					<label>Email</label>
+					<input type = "text" name = "email" required/>
+				</div>
+
+				<div>
+					<label>Password</label>
+					<input type = "text" name = "password" required/>
+				</div>
+
+				<div>
+					<button type = "submit" class="log-in-button">Log In</button>
+				</div>
+			</div>
+		</div>
+
+		
+	</div>
+
+	
+
+		)
+}
+
+export default Login
