e2191c6c66a622375a03e35c9d8a0253ce8ae58a
Brett Kinnamon <b.kinnamon@icloud.com>
2020-06-19 11:25:06 -0400

    add an api usage example component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React from 'react'
+import React from 'react';
 
-import { useApi } from './hooks/useApi';
+import ApiExample from './ApiExample';
 
 import logo from './CYM_logo_v1.svg'
 import './App.css'
 
 function App() {
-  const [result, loading] = useApi('/echo', 'POST', {hello: "World!"});
-
   return (
     <div className="App">
       <img src={logo} className="main-logo" alt="City Youth Matrix logo" />
-      {loading && <pre>Hello {result?.hello}</pre>}
+      <ApiExample />
     </div>
   )
 }
