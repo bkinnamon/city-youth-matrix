0251ecee5308c3c78b894ea5ab5e80c50d8a9b73
Brett Kinnamon <b.kinnamon@icloud.com>
2020-06-23 14:31:30 -0400

    add a view with a list of users

--- src/views/Users.js
+++ src/views/Users.js
@@ -1,16 +1,66 @@
-import React from 'react';
-import { useApi } from '../hooks/useApi';
+import React, { useState, useEffect } from 'react';
+import API from '../helpers/api';
 import headerLogo from '../CYM_logo_v2.svg'
 import NavBar from '../components/NavBar';
+import UserTile from '../components/UserTile';
 
 function Users() {
-  const data = useApi('GET', '/users');
+  const [error, setError] = useState();
+  const [pending, setPending] = useState([]);
+  const [families, setFamilies] = useState([]);
+  const [drivers, setDrivers] = useState([]);
+  const [dispatchers, setDispatchers] = useState([]);
 
-  console.log(data);
+  useEffect(() => {
+    API.callApi('GET', '/users').then(data => {
+      if (data.error) {
+        setError(data.error);
+        return;
+      } else {
+        setPending(data.users.reduce((list, user) => {
+          if (!user.types) list.push(user);
+          else if (user.types.length === 0) list.push(user);
+          return list;
+        }, []));
 
-  const usersBlock = data?.users ?
-    data.users.map(user => <p key={user.username}>Username: {user.username}</p>) :
-    <p>Loading users ...</p>;
+        setFamilies(data.users.reduce((list, user) => {
+          if (user.types && user.types.includes('family')) list.push(user);
+          return list;
+        }, []));
+
+        setDrivers(data.users.reduce((list, user) => {
+          if (user.types && user.types.includes('driver')) list.push(user);
+          return list;
+        }, []));
+
+        setDispatchers(data.users.reduce((list, user) => {
+          if (user.types && user.types.includes('dispatcher')) list.push(user);
+          return list;
+        }, []));
+      }
+    });
+  }, []);
+
+  if (error) {
+    return (
+      <div className="event-page" >
+        <div>
+          <img src={headerLogo} alt="City Youth Matrix logo" className="header-logo" />
+        </div>
+
+        <NavBar />
+
+        <p>{error}</p>
+
+        <div className="event-list">
+          <h2>Users</h2>
+          <div>
+            <p>Loading users ...</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="event-page" >
@@ -22,8 +72,15 @@ function Users() {
 
       <div className="event-list">
         <h2>Users</h2>
-        <div>
-          {usersBlock}
+        <div style={{padding: '0 1rem'}}>
+          <h3>Pending</h3>
+          {pending.map(user => <UserTile key={user.id} user={user} />)}
+          <h3>Families</h3>
+          {families.map(user => <UserTile key={user.id} user={user} />)}
+          <h3>Drivers</h3>
+          {drivers.map(user => <UserTile key={user.id} user={user} />)}
+          <h3>Dispatchers</h3>
+          {dispatchers.map(user => <UserTile key={user.id} user={user} />)}
         </div>
       </div>
     </div>
